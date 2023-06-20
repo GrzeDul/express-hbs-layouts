68418c023b52af6d1a71a2929f7eb3c8db3a17e3
Grzegorz Staszczak <grzestamail@gmail.com>
2023-06-20 13:03:57 +0200

    Add contact form

--- server.js
+++ server.js
@@ -1,12 +1,29 @@
 const express = require('express');
 const path = require('path');
 const hbs = require('express-handlebars');
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, path.join(__dirname, '/public'));
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+    cb(
+      null,
+      file.fieldname + '-' + uniqueSuffix + '.' + file.mimetype.split('/')[1]
+    );
+  },
+});
+
+const upload = multer({ storage: storage });
 
 const app = express();
 app.engine('hbs', hbs({ extname: 'hbs' }));
 app.set('view engine', 'hbs');
 
 app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
   res.render('index');
@@ -32,8 +49,19 @@ app.get('/hello/:name', (req, res) => {
   res.render('hello', { name: req.params.name });
 });
 
+app.post('/contact/send-message', upload.single('design'), (req, res) => {
+  const { author, sender, title, message } = req.body;
+
+  if (author && sender && title && message && req.file) {
+    const fileName = req.file.filename;
+    res.render('contact', { isSent: true, fileName });
+  } else {
+    res.render('contact', { isError: true });
+  }
+});
+
 app.use((req, res) => {
-  res.status(404).show('404.png');
+  res.status(404).render('404');
 });
 
 app.listen(8000, () => {
